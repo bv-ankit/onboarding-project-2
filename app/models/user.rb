43626edf0748220dd08d6ec5129a1c79b1a7e28d
bv-ankit <ankit@blogvault.net>
2020-07-10 12:19:00 +0530

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
   has_many :articles
+  # has_secure_password
+
+  validates :email, 
+    format: { with: URI::MailTo::EMAIL_REGEXP },
+    :uniqueness => { :case_sensitive => true, :message => "is taken" }
+
+  validates :username, 
+    :presence => true,
+    :length => { :in => 4..20 },
+    :uniqueness => { :case_sensitive => false, :message => "is taken" }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :users do
     resources :articles
   end
-  root 'user#new'
+  root 'users#new'
 end
